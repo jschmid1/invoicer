7be7257dab0184469bf15f1e40eb1fc1981ca486
Joshua Schmid <jschmid@suse.com>
2014-12-14 16:16:18 +0100

    add devise secret key

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -11,6 +11,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'admin@wgserv.eu'
+  config.secret_key = ENV['DEVISE_SECRET_KEY'] if Rails.env.production?
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
