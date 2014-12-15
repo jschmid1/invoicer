f15d788755fbd2ed23d25c8fac6747ac0895ac24
Joshua Schmid <jschmid@suse.com>
2014-12-15 17:02:09 +0100

    add monthly statistics method

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -14,4 +14,9 @@ class DashboardController < ApplicationController
     render 'dashboard/graphs'
   end
 
+  def statistics
+    @bills_per_month = Bill.where(created_at: Time.now-30.days..Time.now).where(user_id: User.where(flat_id: current_user.flat_id))
+    render 'dashboard/statistics'
+  end
+
 end
