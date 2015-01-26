5d6253119cd88396c124942790fe9b54a7a6b182
Joshua Schmid <jschmid@suse.com>
2015-01-26 13:39:05 +0100

    adapt RSpec configs and add user spec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe 'User' do
+  it 'will take email address only if matches regex'
+  it 'will force you to put in a name'
+  it 'has a password with more than 8 characters'
+  it 'has a password_confirmation that matches password'
+  it 'could have a custom avatar, but always a default avatar'
+end
