c4ac352cb886bde601dc8d86cb304cad8008d958
Joshua Schmid <jschmid@suse.com>
2015-03-04 16:41:44 +0100

    rework for tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,13 +1,8 @@
 class Task < ActiveRecord::Base
-<<<<<<< Updated upstream
-=======
-
   validates :name, presence: true
   validates :credit, presence: true
   validates :flat_id, presence: true
   belongs_to :flat
   has_many :working_on_tasks
   has_many :users, :through => :working_on_tasks
-  #has_and_belongs_to_many :users, :join_table => "tasks_users"
->>>>>>> Stashed changes
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  require 'yaml'
+  after_create :user_task_entry
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :bills
@@ -9,4 +11,12 @@ class User < ActiveRecord::Base
   has_many :sent_invites, :class_name => "Invite", :foreign_key => 'sender_id'
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
+  def user_task_entry
+    file = 'config/user_tasks.yml'
+    data = YAML::load_file(file)
+    data.merge(User.last.id => [])
+    File.open(file, 'w') {|f| f.write data.to_yaml }
+  end
+
 end
