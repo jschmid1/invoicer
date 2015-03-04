29b72c31c0e9bc014108f31ba07348ed48548b5a
Joshua Schmid <jschmid@suse.com>
2015-03-04 16:44:02 +0100

    remove old stuff

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -95,64 +95,17 @@ class TasksController < ApplicationController
     end
   end
 
-  #def tasks_to_user
-  #  unless check_for_free_users.empty?
-  #    check_for_free_users.each do |user_id|
-  #      unless find_free_tasks.empty?
-  #        if  already_did_this_shit(user_id).map(&:task_id).count >= Task.all.count
-  #          WorkingOnTask.new(user_id: user_id, task_id: already_did_this_shit(user_id).map(&:task_id).first, start_time: calculate_time_from_interval('start', already_did_this_shit(user_id).map(&:task_id).first), end_time: calculate_time_from_interval('end', already_did_this_shit(user_id).map(&:task_id).first)).save
-  #        else
-  #          (find_free_tasks- already_did_this_shit(user_id).map(&:task_id)).each do |task_id|
-  #            WorkingOnTask.new(user_id: user_id, task_id: task_id, start_time: calculate_time_from_interval('start', task_id), end_time: calculate_time_from_interval('end', task_id)).save
-  #            break###
-  #
-  #           end
-  #        end
-  #     end
-  #  end
-  #end
-  # scopes... ZOMG
-  # done button
-  # calculate credits based on relation table
-  # only give credit if done&&end_time
-
-  # end
-
-  # The tasks that the specified user[(user_id)] has already done and should not do again.
-  ##  def already_did_this_shit(user_id)
-  #   @ids =[]
-  #   WorkingOnTask.where(user_id: user_id).order(:created_at).limit(Task.all.count).each do |task|
-  #     if @ids.include?(task.task_id)
-  #
-  #      else
-  #       @ids.append(task.id) unless @ids.include?(user_id)
-  #     end
-
-  #  end
-  # if @ids.count >= Task.all.count pick first of list
-  #
-  # end
-  #get all tasks from user X
-  #sort it by creation date
-  #map task_ids until mapped_id already is in array.
-  # subract mapped_ids from free_tasks.
-  #end
-
-
   def calculate_time_from_interval(start_or_stop, task_id)
     case Task.find_by(id: task_id).interval
       when 'Every 2 Weeks'
         return Time.now if start_or_stop == 'start'
-        return Time.now + 1.minutes if start_or_stop == 'end'
-      # return Time.now + 2.weeks if start_or_stop == 'end'
+        return Time.now + 2.weeks if start_or_stop == 'end'
       when 'Every Day'
         return Time.now if start_or_stop == 'start'
-        return Time.now + 1.minutes if start_or_stop == 'end'
-      # return Time.now + 1.days if start_or_stop == 'end'
+        return Time.now + 1.days if start_or_stop == 'end'
       when 'Every Week'
         return Time.now if start_or_stop == 'start'
-        # return Time.now + 1.weeks if start_or_stop == 'end'
-        return Time.now + 1.minutes if start_or_stop == 'end'
+        return Time.now + 1.weeks if start_or_stop == 'end'
       when 'Every 3 Weeks'
         return Time.now if start_or_stop == 'start'
         return Time.now + 3.weeks if start_or_stop == 'end'
