333634fb7e07ffca14467b03175244d640419138
Joshua Schmid <jschmid@suse.com>
2015-03-04 16:51:06 +0100

    assoc with tasks

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,10 @@ class User < ActiveRecord::Base
   belongs_to :flat
   has_many :invitations, :class_name => "Invite", :foreign_key => 'recipient_id'
   has_many :sent_invites, :class_name => "Invite", :foreign_key => 'sender_id'
+  has_many :working_on_tasks
+  has_many :tasks, :through => :working_on_tasks
+  has_and_belongs_to_many :users, :join_table => "tasks_users"
+
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
